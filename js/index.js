101caf0eb04255c957f82ab47e05d14fe4dc36b5
Valeriya <yazikovavleriya15@gmail.com>
2025-12-25 16:17:25 +0400

    about

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.about__slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 34,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
