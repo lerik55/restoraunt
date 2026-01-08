29993644bcc6bb36c8cd2e7cb6a9df927c941aee
Morqqulis <morqqulis@gmail.com>
2026-01-08 16:05:04 +0400

    Tadaaa

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const swiper = new Swiper('.about__slider', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 2,
-    spaceBetween: 34,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+   // Optional parameters
+   loop: true,
+   slidesPerView: 2,
+   spaceBetween: 34,
+   // Navigation arrows
+   navigation: {
+      nextEl: '.about__prev',
+      prevEl: '.about__next',
+   },
 
-});
+})
